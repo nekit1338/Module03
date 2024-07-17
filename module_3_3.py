4f3da68f1a10b655c2a9ac3cf050e6475aae22b6
nekit1338 <47562425+nekit1338@users.noreply.github.com>
2024-07-18 00:29:19 +0500

    Add files via upload

---
+++ module_3_3.py
@@ -0,0 +1,22 @@
+def print_params(a=1, b='строка', c=None):
+    if c is None:
+        c = []
+    print(a, b, c)
+
+
+print_params(1, 2, 3)
+print_params(1, 2)
+print_params(a=2, c=False)
+print_params(b="string", c=2)
+print_params()
+print_params(b=25)
+print_params(c=[4, 5, 6])
+
+values_list = [5, "Abrakadabra", tuple()]
+values_dict = {"a": 5, "b": tuple(), "c": frozenset()}
+values_list_2 = [20, False]
+values_list_3 = [1, "string", False, True]
+print_params(*values_list)
+print_params(**values_dict)
+print_params(42, *values_list_2)
+print_params(*values_list_2, 42)
